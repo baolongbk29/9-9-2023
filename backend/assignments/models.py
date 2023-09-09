0dd8234e55e043cff287e592d24464442fcaa8c1
longlb1 <longlb@seatechit.com.vn>
2023-09-09 10:38:31 +0700

    9-9-2023

---
+++ backend/assignments/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Assignment(models.Model):
+
+    first_term = models.DecimalField(
+        max_digits=5, decimal_places=2, null=False, blank=False
+    )
+
+    second_term = models.DecimalField(
+        max_digits=5, decimal_places=2, null=False, blank=False
+    )
+
+    sum = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True
+    )
